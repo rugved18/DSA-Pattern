f024845b160e8bac4e1caeb2a4fd261942efa353
surajgojaur <surajgojanur@gmail.com>
2025-09-29 18:47:18 +0530

    worked on two pointer

---
+++ Two_pointer/Suraj/11. Container With Most Water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0,right=height.length-1;
+        int result=0;
+        while(left<right){
+            int curMin=Math.min(height[left],height[right]);
+            int curArea=curMin*(right-left);
+            result=Math.max(result,curArea);
+            if(height[left]<height[right]){
+                left++;
+            }
+            else if(height[left]>height[right]){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return result;
+    }
+}
